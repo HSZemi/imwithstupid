c388643a3b66ec264ebe86295942f1e2f4316476
HSZemi <HSZemi@users.noreply.github.com>
2013-05-14 18:36:16 +0200

    test works now with PHP 5.2

--- admin/dbtest.php
+++ admin/dbtest.php
@@ -1,4 +1,3 @@
- 
 <html>
 <head><title>iws testsuite</title>
 <meta http-equiv="content-type" content="text/html; charset=utf-8">
@@ -13,7 +12,7 @@ include '../lib/db.php';
 // mit Datenbank verbinden
 $conn = db_connect();
 
-$players = ['Lieschen Müller','hannskasper','t-bone','myrabilis','katreina','Erno der Erno','HSZemi'];
+$players = array('Lieschen Müller','hannskasper','t-bone','myrabilis','katreina','Erno der Erno','HSZemi');
 
 foreach($players as $i){
 	create_player($i);
@@ -21,7 +20,7 @@ foreach($players as $i){
 
 create_round(2);
 
-$questions_2 = ['Nenne eine beliebte Farbe für Ostereier',
+$questions_2 = array('Nenne eine beliebte Farbe für Ostereier',
 'Nenne eine Tätigkeit im Schnee außer Ski alpin/Snowboarden',
 'Nenne einen beliebten Weihnachtsmarkt',
 'Nenne eine fiktionale Figur im Zusammenhang mit Weihnachten/Geschichte über Weihnachten',
@@ -30,7 +29,7 @@ $questions_2 = ['Nenne eine beliebte Farbe für Ostereier',
 'Nenne eine Nuss, die vor allem in der Vorweihnachtszeit gegessen wird',
 'Nenne einen Gletscher in Österreich',
 'Nenne eine beliebte Tätigkeit im Frühjahr',
-'Nenne eine typische Veranstaltung im Mai'];
+'Nenne eine typische Veranstaltung im Mai');
 
 $nr = 1;
 foreach($questions_2 as $i){
@@ -38,25 +37,25 @@ foreach($questions_2 as $i){
 	$nr++;
 }
 
-$answers[0] = ['rot','grün','gelb','pink','bunt','blau'];
+$answers[0] = array('rot','grün','gelb','pink','bunt','blau');
 
-$answers[1]= ['rodeln','Schneeballschlacht','Schneemann bauen','Langlauf'];
+$answers[1] = array('rodeln','Schneeballschlacht','Schneemann bauen','Langlauf');
 
-$answers[2] = ['Nürnberger Christkindlmarkt','Dresdner Striezelmarkt','Stuttgarter Christkindlmarkt'];
+$answers[2] = array('Nürnberger Christkindlmarkt','Dresdner Striezelmarkt','Stuttgarter Christkindlmarkt');
 
-$answers[3] = ['Weihnachtsmann','Jesus','Ebenezer Scrooge','Gringe','Christkind','Der kleine Lord'];
+$answers[3] = array('Weihnachtsmann','Jesus','Ebenezer Scrooge','Gringe','Christkind','Der kleine Lord');
 
-$answers[4] = ['Drei Nüsse für Aschenbrödel','Der kleine Lord','Dinner for one','Sissi','Kevin allein zuhaus','Santa Claus','Das Wunder von Manhattan'];
+$answers[4] = array('Drei Nüsse für Aschenbrödel','Der kleine Lord','Dinner for one','Sissi','Kevin allein zuhaus','Santa Claus','Das Wunder von Manhattan');
 
-$answers[5] = ['Fondue','Raclette','Karpfen','Nudelsalat','Sekt'];
+$answers[5] = array('Fondue','Raclette','Karpfen','Nudelsalat','Sekt');
 
-$answers[6] = ['Walnuss','Haselnuss','Mandel','Erdnuss 1'];
+$answers[6] = array('Walnuss','Haselnuss','Mandel','Erdnuss 1');
 
-$answers[7] = ['Stubai','Hintertux','Kitzsteinhorn','Dachstein','Kaunertal','Sölden'];
+$answers[7] = array('Stubai','Hintertux','Kitzsteinhorn','Dachstein','Kaunertal','Sölden');
 
-$answers[8] = ['Frühjahrsputz','Gartenarbeit','Spazieren','Wandern','Radfahren','an der Bikinifigur arbeiten'];
+$answers[8] = array('Frühjahrsputz','Gartenarbeit','Spazieren','Wandern','Radfahren','an der Bikinifigur arbeiten');
 
-$answers[9] = ['Maibaum aufstellen','Tanz in den Mai','1. Mai in Kreuzberg','Vatertag','Walpurgisnacht','Muttertag','Maifeuer','TomFrasers Geburtstag'];
+$answers[9] = array('Maibaum aufstellen','Tanz in den Mai','1. Mai in Kreuzberg','Vatertag','Walpurgisnacht','Muttertag','Maifeuer','TomFrasers Geburtstag');
 
 for($i = 0; $i < 10; $i++){
 	foreach($answers[$i] as $item){
@@ -72,15 +71,15 @@ create_answer('Wie alt sind Sie?', 'über 10');
 
 
 add_answer_string('katreina', 'Nenne eine beliebte Farbe für Ostereier','grün');
-add_answer_string('katreina', '2. Nenne eine Tätigkeit im Schnee außer Ski alpin/Snowboarden','Schneeballschlacht');
-add_answer_string('katreina', '3. Nenne einen beliebten Weihnachtsmarkt','Nürnberger Christkindlmarkt');
-add_answer_string('katreina', '4. Nenne eine fiktionale Figur im Zusammenhang mit Weihnachten/Geschichte über Weihnachten','Jesus');
-add_answer_string('katreina', '5. Nenne einen Film der im Dezember immer im Fernsehen läuft','Drei Nüsse für Aschenbrödel');
-add_answer_string('katreina', '6. Nenne ein typisches Silvestergericht','Raclette');
-add_answer_string('katreina', '7. Nenne eine Nuss, die vor allem in der Vorweihnachtszeit gegessen wird','Haselnuss');
-add_answer_string('katreina', '8. Nenne einen Gletscher in Österreich','Kitzsteinhorn');
-add_answer_string('katreina', '9. Nenne eine beliebte Tätigkeit im Frühjahr','Frühjahrsputz');
-add_answer_string('katreina', '10. Nenne eine typische Veranstaltung im Mai', 'Walpurgisnacht');
+add_answer_string('katreina', 'Nenne eine Tätigkeit im Schnee außer Ski alpin/Snowboarden','Schneeballschlacht');
+add_answer_string('katreina', 'Nenne einen beliebten Weihnachtsmarkt','Nürnberger Christkindlmarkt');
+add_answer_string('katreina', 'Nenne eine fiktionale Figur im Zusammenhang mit Weihnachten/Geschichte über Weihnachten','Jesus');
+add_answer_string('katreina', 'Nenne einen Film der im Dezember immer im Fernsehen läuft','Drei Nüsse für Aschenbrödel');
+add_answer_string('katreina', 'Nenne ein typisches Silvestergericht','Raclette');
+add_answer_string('katreina', 'Nenne eine Nuss, die vor allem in der Vorweihnachtszeit gegessen wird','Haselnuss');
+add_answer_string('katreina', 'Nenne einen Gletscher in Österreich','Kitzsteinhorn');
+add_answer_string('katreina', 'Nenne eine beliebte Tätigkeit im Frühjahr','Frühjahrsputz');
+add_answer_string('katreina', 'Nenne eine typische Veranstaltung im Mai', 'Walpurgisnacht');
 add_answer_string('katreina', 'Wie alt sind Sie?', 'unter 10');
 
 add_answer_number('HSZemi', 2, 1, 'grün');
